f785a7bb0bd13fafb0df333298f40475a9e26a37
Jennifer Gingras <jgingras@meca.edu>
2019-04-20 20:51:27 -0400

    routes and buttons

---
+++ public/assets/logic.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+  console.log("hi there!");
+
+  // Methods for Requests
+  var API = {
+    saveBurger: function (burger) {
+      return $.ajax({
+        headers: {
+          "Content-Type": "application/json"
+        },
+        type: "POST",
+        url: "api/burgers",
+        data: JSON.stringify(burger)
+      });
+    },
+  };
+
+
+  // Functions
+  // add burger
+  function addBurger() {
+    var newBurger = {
+      burger_name: $('#burger-input').val().trim(),
+    };
+    console.log(newBurger),
+      API.saveBurger(newBurger).then(function (data) {
+        console.log("new burger added");
+        console.log(data);
+      });
+  }
+
+
+  // BUTONS
+  $()
+
+});

b4aa495816ccb5910d1f5c3ee13e1ee51a54081f
Jennifer Gingras <jgingras@meca.edu>
2019-04-21 23:12:27 -0400

    Styling

--- routing/apiroutes.js
+++ routing/apiroutes.js
@@ -1,25 +1,14 @@
 var express = require('express');
-var burger = require("../models");
+var db = require("../models");
 
 
 module.exports = function (app) {
   // display all burgers 
-  app.get("/burgers", function (req, res) {
+  app.get("/api/burgers", function (req, res) {
     db.Burger.findAll({}).then(function (dbBurger) {
       res.json(dbBurger);
     });
   });
-
-  // router.get("/", function (req, res) {
-  //   burger.all(function (data) {
-  //     var hbsObject = {
-  //       burgers: data
-  //     };
-  //     console.log(hbsObject);
-  //     res.render("index", hbsObject);
-  //   });
-  // });
-
 
 
   // add burger
@@ -30,13 +19,13 @@ module.exports = function (app) {
   });
 
 
-
+  // change burger state
   app.put("/api/burgers/:id", function (req, res) {
     var condition = "id = " + req.params.id;
 
     console.log("condition", condition);
 
-    burger.update({
+    db.Burger.update({
       devoured: req.body.devoured
     }, condition, function (result) {
       if (result.changedRows == 0) {
@@ -51,7 +40,7 @@ module.exports = function (app) {
   app.delete("/api/burgers/:id", function (req, res) {
     var condition = "id = " + req.params.id;
 
-    burger.delete(condition, function (result) {
+    db.Burger.delete(condition, function (result) {
       if (result.affectedRows == 0) {
         // If no rows were changed, then the ID must not exist, so 404
         return res.status(404).end();
@@ -63,3 +52,4 @@ module.exports = function (app) {
 
 
 };
+

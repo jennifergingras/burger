9cfd0274029133caa3afd45dcd44e319f3bebb80
Jennifer Gingras <jgingras@meca.edu>
2019-04-24 16:32:07 -0400

    heroku

--- models/index.js
+++ models/index.js
@@ -8,9 +8,11 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
+console.log(config);
+
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL, config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
